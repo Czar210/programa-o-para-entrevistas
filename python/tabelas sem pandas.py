bd65a269007028fbebc4cf49bcd159ab6e40353a
César Augusto Sibila <cesarsibila210@gmail.com>
2026-03-27 15:03:43 -0300

    Update tabelas sem pandas.py

--- python/tabelas sem pandas.py
+++ python/tabelas sem pandas.py
@@ -6,15 +6,16 @@ funcionarios = [
     {"nome": "Eva", "depto": "Engenharia", "salario": 7200},
 ]
 
-media_por_departamento(lista):
+def media_por_departamento(lista):
     acumulador = {}
-    for funcionario in funcionarios: 
+    for funcionario in lista: 
+        departamento = funcionario["depto"]
         if departamento not in acumulador:
-            acumulador[departamento] = {"total": 0, count: 0}
-        acumulador[departamento][total] += funcionario["salario"]
-        acumulador[departamento][count] += 1
+            acumulador[departamento] = {"total": 0, "count": 0}
+        acumulador[departamento]["total"] += funcionario["salario"]
+        acumulador[departamento]["count"] += 1
 
-        media = {departamento: acumulador[departamento][total] / acumulador[departamento][count] for departamento in acumulador}
+        media = {departamento: acumulador[departamento]["total"] / acumulador[departamento]["count"] for departamento in acumulador}
     return media
 
 print(media_por_departamento(funcionarios))
